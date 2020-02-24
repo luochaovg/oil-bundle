cd763f927363faeb5a694fc91581f058f7559f31
echo <echo@echos-MacBook-Pro.local>
2020-02-24 18:19:24 +0800

    perfect

--- Entity/BswCommandQueue.php
+++ Entity/BswCommandQueue.php
@@ -24,6 +24,9 @@ class BswCommandQueue extends FoundationEntity
      * @ORM\Column(type="integer", name="`id`")
      * @Assert\Type(type="integer", groups={"modify"})
      * @Assert\NotNull(groups={"modify"})
+     * @BswAnnotation\Preview(sort=1, align="center", width=90, render=BswAbs::RENDER_CODE)
+     * @BswAnnotation\Persistence(sort=1, type=BswForm\Number::class)
+     * @BswAnnotation\Filter(sort=1, type=BswForm\Number::class)
      */
     protected $id;
 
@@ -32,12 +35,18 @@ class BswCommandQueue extends FoundationEntity
      * @Assert\Type(type="string", groups={"modify", "newly"})
      * @Assert\NotNull(groups={"modify", "newly"})
      * @Assert\Length(max=128, groups={"modify", "newly"})
+     * @BswAnnotation\Preview(sort=2, render=BswAbs::HTML_TEXT, enumExtra=true)
+     * @BswAnnotation\Persistence(sort=2, type=BswForm\Select::class, enumExtra=true)
+     * @BswAnnotation\Filter(sort=2, type=BswForm\Select::class, enumExtra=true)
      */
     protected $command;
 
     /**
      * @ORM\Column(type="string", name="`condition`")
      * @Assert\Type(type="string", groups={"modify", "newly"})
+     * @BswAnnotation\Preview(sort=3, width=360, hook={0:BswHook\JsonStringify::class}, render=BswAbs::HTML_PRE)
+     * @BswAnnotation\Persistence(sort=3, hook={0:BswHook\JsonStringify::class}, type=BswForm\TextArea::class)
+     * @BswAnnotation\Filter(sort=3)
      */
     protected $condition;
 
@@ -45,6 +54,9 @@ class BswCommandQueue extends FoundationEntity
      * @ORM\Column(type="smallint", name="`resource_need`")
      * @Assert\Type(type="integer", groups={"modify"})
      * @Assert\NotNull(groups={"modify"})
+     * @BswAnnotation\Preview(sort=4, align="center", render="{value} %")
+     * @BswAnnotation\Persistence(sort=4, type=BswForm\Slider::class, typeArgs={"max":50, "marks": {10:10, 20:20, 30:30, 40:40}})
+     * @BswAnnotation\Filter(sort=4, type=BswForm\Number::class)
      */
     protected $resourceNeed = 1;
 
@@ -52,6 +64,9 @@ class BswCommandQueue extends FoundationEntity
      * @ORM\Column(type="float", name="`done_percent`")
      * @Assert\Type(type="numeric", groups={"modify"})
      * @Assert\NotNull(groups={"modify"})
+     * @BswAnnotation\Preview(sort=5, width=140, align="center", render="{value} %")
+     * @BswAnnotation\Persistence(sort=5, show=false)
+     * @BswAnnotation\Filter(sort=5)
      */
     protected $donePercent = 0;
 
@@ -60,6 +75,9 @@ class BswCommandQueue extends FoundationEntity
      * @Assert\Type(type="string", groups={"modify"})
      * @Assert\NotNull(groups={"modify"})
      * @Assert\Length(max=128, groups={"modify"})
+     * @BswAnnotation\Preview(sort=6, render=BswAbs::HTML_PRE, width=360)
+     * @BswAnnotation\Persistence(sort=6, type=BswForm\TextArea::class)
+     * @BswAnnotation\Filter(sort=6)
      */
     protected $remark = "";
 
@@ -67,6 +85,9 @@ class BswCommandQueue extends FoundationEntity
      * @ORM\Column(type="string", name="`add_time`")
      * @Assert\Type(type="string", groups={"modify"})
      * @Assert\NotNull(groups={"modify"})
+     * @BswAnnotation\Preview(sort=7, align="center", render=BswAbs::RENDER_CODE, width=190)
+     * @BswAnnotation\Persistence(sort=7, show=false, type=BswForm\Datetime::class)
+     * @BswAnnotation\Filter(sort=7, type=BswForm\DatetimeRange::class, column=4, filter=BswFilter\Between::class)
      */
     protected $addTime;
 
@@ -74,6 +95,9 @@ class BswCommandQueue extends FoundationEntity
      * @ORM\Column(type="string", name="`update_time`")
      * @Assert\Type(type="string", groups={"modify"})
      * @Assert\NotNull(groups={"modify"})
+     * @BswAnnotation\Preview(sort=8, align="center", render=BswAbs::RENDER_CODE, width=190)
+     * @BswAnnotation\Persistence(sort=8, show=false, type=BswForm\Datetime::class)
+     * @BswAnnotation\Filter(sort=8, type=BswForm\DatetimeRange::class, column=4, filter=BswFilter\Between::class)
      */
     protected $updateTime;
 
@@ -81,6 +105,9 @@ class BswCommandQueue extends FoundationEntity
      * @ORM\Column(type="smallint", name="`state`")
      * @Assert\Type(type="integer", groups={"modify"})
      * @Assert\NotNull(groups={"modify"})
+     * @BswAnnotation\Preview(sort=9, align="center", enum=true, dress={0:"default", 1:"processing"}, status=true)
+     * @BswAnnotation\Persistence(sort=9, type=BswForm\Select::class, enum=true)
+     * @BswAnnotation\Filter(sort=9, type=BswForm\Select::class, enum=true)
      */
     protected $state = 1;
 }
