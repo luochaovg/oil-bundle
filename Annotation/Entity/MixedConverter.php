5df7df64877134acbf39b3dfcc4365eff2a06e5c
rinin <rinin>
2020-04-30 11:38:07 +0800

    perfect

--- Annotation/Entity/MixedConverter.php
+++ Annotation/Entity/MixedConverter.php
@@ -3,18 +3,6 @@
 namespace Leon\BswBundle\Annotation\Entity;
 
 use Leon\BswBundle\Annotation\AnnotationConverter;
-use Leon\BswBundle\Annotation\Entity\Traits\EnumConverter;
-use Leon\BswBundle\Annotation\Entity\Traits\EnumExtraConverter;
-use Leon\BswBundle\Annotation\Entity\Traits\EnumHandlerConverter;
-use Leon\BswBundle\Annotation\Entity\Traits\FormTypeArgsConverter;
-use Leon\BswBundle\Annotation\Entity\Traits\FormTypeConverter;
-use Leon\BswBundle\Annotation\Entity\Traits\HookConverter;
-use Leon\BswBundle\Annotation\Entity\Traits\TransConverter;
-use Leon\BswBundle\Component\Helper;
-use Leon\BswBundle\Module\Filter\Entity\Accurate;
-use Leon\BswBundle\Module\Filter\Entity\Like;
-use Symfony\Component\Validator\Constraints\Type;
-use Leon\BswBundle\Module\Filter\Filter as BswFilter;
 
 /**
  * @property Mixed $item
